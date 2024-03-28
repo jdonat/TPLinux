8032efdde702eb6b1e96e0ed0d60d83afc30adec
John Doe <150659229+jdonat@users.noreply.github.com>
2024-03-28 10:15:15 +0100

    Test 8

--- bash.sh
+++ bash.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-mode=$1
-username=$2
-password=$3
-site=$username
-
 function createUser() {
 	echo "user: $1 created with password: $2"
 	useradd -p "$2" "$1"
@@ -19,20 +14,29 @@ function updateRepo(){
 }
 
 function config_site(){
-	cp /etc/nginx/sites-available/default /etc/nginx/sites-available/$1
+	cp ./template.conf /etc/nginx/sites-available/$1
+ 	export HTTP_PORT =$2
+  	export SITE_NAME = $1
+	envsubst < /etc/nginx/sites-available/$1
 }
 
-case $mode in
+function get_template_config() {
+	cp /etc/nginx/sites-available/default ./template.conf
+}
+
+case $1 in
 	"user")
 	createUser $username $password
 	;;
-
 	"install")
 	updateRepo
 	;;
 	"configure_site")
-	config_site $site
+	config_site $2 $3
 	;;
+ 	"template")
+  	get_template_config
+   	;;
 	*)
 	echo -e "Erreur : le 1er parametre passé au script doit être ∕nuser pour crer un nouvel utilisateur∕nou install pour mettre a jour les repositories et installer nginx"
 	;;
